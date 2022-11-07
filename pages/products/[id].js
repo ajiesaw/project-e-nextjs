54fe679532ff5785b4a041a44520ba0cf82c7989
ajiesaw <ajiesaw1612@gmail.com>
2022-11-07 11:46:37 +0700

    Mengimport component color dan size

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,5 +1,7 @@
 import Navbar from "../../components/navbar"
 import Counter from "../../components/counter"
+import Color from "../../components/color"
+import Size from "../../components/size"
 import { product } from '../../data'
 import Head from 'next/head'
 import Link from "next/link";
@@ -89,27 +91,9 @@ const [size, setSize] = useState('5l')
             <div className="border-b-2 py-2"></div>
 
             <div className="flex pt-4 gap-10">
-              <div>
-                <p className="py-2">Color</p>
-                  <div className="flex space-x-3">
-                    <button className={`w-7 h-7 bg-blue-900 rounded-full hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${color === 'blue' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setColor('blue')}></button>
-                    <button className={`w-7 h-7 bg-red-900 rounded-full hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${color === 'red' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setColor('red')}></button>
-                    <button className={`w-7 h-7 bg-green-900 rounded-full hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${color === 'green' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setColor('green')}></button>
-                    <button className={`w-7 h-7 bg-yellow-900 rounded-full hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${color === 'yellow' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setColor('yellow')}></button>
-                    <button className={`w-7 h-7 bg-purple-900 rounded-full hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${color === 'purple' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setColor('purple')}></button>
-                  </div>
-              </div>
+              <Color />
 
-              <div>
-                <p className="py-2">Size</p>
-                  <div className="flex space-x-3">
-                    <button className={`w-7 h-7 bg-gray-100 rounded-md text-sm hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${size === '5L' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setSize('5L')}>5L</button>
-                    <button className={`w-7 h-7 bg-gray-100 rounded-md text-sm hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${size === '8L' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setSize('8L')}>8L</button>
-                    <button className={`w-7 h-7 bg-gray-100 rounded-md text-sm hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${size === '10L' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setSize('10L')}>10L</button>
-                    <button className={`w-7 h-7 bg-gray-100 rounded-md text-sm hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${size === '13L' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setSize('13L')}>13L</button>
-                    <button className={`w-7 h-7 bg-gray-100 rounded-md text-sm hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${size === '15L' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setSize('15L')}>15L</button>
-                  </div>
-              </div>
+              <Size />
             </div>
 
             <div className="flex pt-10">
