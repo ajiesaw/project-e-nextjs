710025f3144337b7bbe4e675494ed8f448bdbcc2
ajiesaw <ajiesaw1612@gmail.com>
2022-11-07 15:16:16 +0700

    Menetapkan state awal menjadi 1

--- components/counter.js
+++ components/counter.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
   
 const Counter = () => {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(1)
   
   const handleClick1 = () => {
     setCounter(counter + 1)
