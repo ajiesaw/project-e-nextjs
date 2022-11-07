64d6d86e0c64e1501de78effde3f2236ab82e073
ajiesaw <ajiesaw1612@gmail.com>
2022-11-07 12:36:56 +0700

    Menambahkan padding

--- components/size.js
+++ components/size.js
@@ -4,8 +4,8 @@ const Size = () => {
   const [size, setSize] = useState()
 
   return (
-    <div>
-      <p className="py-2">Size</p>
+    <div className="py-6">
+      <p className="pb-2">Size</p>
         <div className="flex space-x-3">
           <button className={`w-7 h-7 bg-gray-100 rounded-md text-sm hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${size === '5L' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setSize('5L')}>5L</button>
           <button className={`w-7 h-7 bg-gray-100 rounded-md text-sm hover:ring-2 hover:ring-offset-1 hover:ring-gray-400 ${size === '8L' ? `focus:ring-2 focus:ring-offset-1 ring-gray-400` : ``}`} onClick={()=> setSize('8L')}>8L</button>
